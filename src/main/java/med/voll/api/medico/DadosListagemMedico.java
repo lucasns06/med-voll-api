71fad37e602a2844052fe9ca427bdc7065cbd13c
lucasns06 <lucasnascimento.carreira@gmail.com>
2025-08-11 14:44:11 -0300

    upload

---
+++ src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -0,0 +1,15 @@
+package med.voll.api.medico;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+
+    public DadosListagemMedico(Medico medico) {
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
